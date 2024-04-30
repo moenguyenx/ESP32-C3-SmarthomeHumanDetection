964e21d6cf3d45786edccd32d3a956f9e3f63311
moenguyenx <qminh1908@gmail.com>
2024-04-30 11:41:12 +0700

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Overview
+This is Microprocessors (ET3300) Mid-term Project.
+
+Executed by [Nguyen Quang Minh](https://www.facebook.com/moenguyenx) - 20214010
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "humanDetect.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
